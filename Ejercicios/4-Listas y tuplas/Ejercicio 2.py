1858ad5c65d75951374797d6215c18c34e137ecd
Aitor López <aitorlopez98@opendeusto.es>
2020-03-11 23:52:23 +0100

    diccionario ej 1

--- Ejercicios/4-Listas y tuplas/Ejercicio 2.py
+++ Ejercicios/4-Listas y tuplas/Ejercicio 2.py
@@ -2,19 +2,22 @@ i = 0
 j = 0
 lista = []
 lista2 = []
+comparacion = []
 
 while i < 5:
-    num = int(input("escribe un nuemro"))
+    num = int(input("escribe un numero"))
     lista.append(num)
     i += 1
-print(lista)
+print(f"Primera lista {lista}")
 
 while j < 5:
-    num = int(input("escribe un nuemro"))
+    num = int(input("escribe un numero"))
     lista2.append(num)
     j += 1
-print(lista2)
+print(f"Segunda lista {lista2}")
 
 for n in lista:
-    for k in lista2:
-        
+    if n in lista2:
+        comparacion.append(n)
+
+print(comparacion)
