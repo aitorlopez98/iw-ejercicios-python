149cfbe186a140616920e1325d51d87d269095e4
Aitor López <aitorlopez98@opendeusto.es>
2020-03-17 13:32:05 +0100

    clases ej 2 fin

---
+++ Ejercicios/8-Clases y objetos/Ejercicio 2.py
@@ -0,0 +1,41 @@
+class clsRobot:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def avanzar(self):
+        self.x += 1
+
+    def retorceder(self):
+        self.x -= 1
+
+    def izquierda(self):
+        self.y -= 1
+
+    def derecha(self):
+        self.y += 1
+
+    def posicion(self):
+        _x = self.x
+        _y = self.y
+        print(f"La posicción (X,Y) es ({_x},{_y})")
+
+r1 = clsRobot(0, 0)
+move = ""
+
+while move != "fin":
+    move = input("A-adelante/R-retroceder/I-izquierda/D-derecha/FIN-salir: ")
+    move = move.lower()
+    if move.__eq__("a"):
+        r1.avanzar()
+
+    elif move.__eq__("r"):
+        r1.retorceder()
+
+    elif move.__eq__("i"):
+        r1.izquierda()
+
+    elif move.__eq__("d"):
+        r1.derecha()
+
+    r1.posicion()
